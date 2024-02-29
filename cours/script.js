60caf33dfd8a08cb104b371bd22d02cfdee53b6a
YouctoDEV <youcto.github@gmail.com>
2024-02-29 23:20:15 +0100

    ...

---
+++ cours/script.js
@@ -0,0 +1,22 @@
+// let = Une variablee qui peut changer
+let Age = 15 + "ans ";
+let Age1 = 16 + "ans ";
+let Age2 = 17 + "ans ";
+// const = Une variable qui changera jamais
+const Info = "Enzo Bourdel : " + Age + Age1 + Age2; // const Info1 = "Enzo Bourdel : " + Age + Age1 + Age2;
+
+let Result = 16;
+// Result = Result + 1;
+    Result += 2;
+// Result = Result * 2
+    Result *= 2;
+
+let x = 2;
+let y = 1;
+
+if (x > y) {
+    alert("x est plus grand que y !")
+} else {
+    alert("x est plus petit que y !")
+}
+
---
+++ index.js
@@ -0,0 +1,45 @@
+//QUIZZ
+
+const QuestionContainer = document.querySelector('.click-event');
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.getElementById('btn-2')
+const reponse = document.querySelector('p');
+
+QuestionContainer.addEventListener('click', () => {
+    QuestionContainer.classList.toggle('question-click');
+});
+
+btn1.addEventListener('click', () => {
+    reponse.classList.add('reponse')
+    reponse.style.background = "green";
+})
+
+btn2.addEventListener('click', () => {
+    reponse.classList.add('reponse')
+    reponse.style.background = "red";
+})
+
+//SOURIS
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+    mousemove.style.left = e.pageX + "px";
+    mousemove.style.top = e.pageY + "px";
+
+});
+
+window.addEventListener("mousedown", () => {
+    // console.log("TEST")
+})
+
+const keypressContainer = document.querySelector('.keypress');
+const key = document.getElementById('key');
+
+document.addEventListener('keypress', (e) => {
+    key.textContent = e.key;
+    if( e.key === "f") {
+        keypressContainer.style.background = "orange";
+    } else {
+        keypressContainer.style.background = "#333"
+    }
+})
